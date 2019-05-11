28f4bc9e3a721a2f15e4a4399eb054902c590481
jimmy feriawan <jimyferi@gmail.com>
2019-05-11 09:21:08 +0700

    sampai ke grayscale

---
+++ src/pengolahancitra/PengolahanCitra.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pengolahancitra;
+
+/**
+ *
+ * @author jimmyferiawan
+ */
+public class PengolahanCitra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("halo");
+    }
+    
+}
